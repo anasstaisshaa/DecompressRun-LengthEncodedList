12d87eb224b2e2472b6461355097cec578f861a5
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-03-31 20:45:37 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> list = new ArrayList();
+        for(int i = 0; i  < nums.length; i += 2){
+            for(int j = 0; j < nums[i]; j++){
+                list.add(nums[i+1]);
+            }
+        }
+        //int[] ans = list.stream().mapToInt(i -> i).toArray();
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+}
